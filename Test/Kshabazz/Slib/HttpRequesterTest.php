db79221f59adc780b98061d52bfa65fcc368d4da
Khalifah Shabazz <shabazzk@gmail.com>
2014-03-16 23:49:27 -0400

    Added HttpRequester class for making HTTP request.

---
+++ Test/Kshabazz/Slib/HttpRequesterTest.php
@@ -0,0 +1,91 @@
+<?php namespace Kshabazz\Slib\Test;
+/**
+ * Developers: Khalifah K. Shabazz
+ */
+use Kshabazz\Slib\HttpRequester;
+/**
+ * Class HttpRequesterTest
+ *
+ * @package Kshabazz\Slib\Test
+ */
+class HttpRequesterTest extends \PHPUnit_Framework_TestCase
+{
+	private
+		$url;
+
+	public function setUp()
+	{
+		$this->url = 'http://www.example.com';
+	}
+
+	public function test_initialization()
+	{
+		$httpRequester = new HttpRequester( NULL );
+		$this->assertInstanceOf(
+			'\Kshabazz\Slib\HttpRequester',
+			$httpRequester,
+			'Could not initialize \Kshabazz\Slib\HttpRequester object.'
+		);
+	}
+
+	public function test_getting_the_same_url_put_in()
+	{
+		$httpRequester = new HttpRequester( $this->url );
+		$url = $httpRequester->url();
+		$this->assertEquals( $this->url, $url, 'URL retrieved is not the same as the one put in.' );
+	}
+
+	public function test_updating_url_with_setUrl()
+	{
+		$secondUrl = 'http://www.kshabazz.net';
+		$httpRequester = new HttpRequester( $this->url );
+		$url = $httpRequester->url();
+		$this->assertEquals( $this->url, $url, 'URL retrieved is not the same as the one put in.' );
+		$httpRequester->setUrl( $secondUrl );
+		$url = $httpRequester->url( $secondUrl );
+		$this->assertEquals( $secondUrl, $url, 'The second URL retrieved is not the same as the second URL set.' );
+	}
+
+	public function test_setting_headers()
+	{
+		$testCase = 'Content-type: text/xml';
+		$httpRequester = new HttpRequester( NULL );
+		$httpRequester->headers([ $testCase ]);
+		$curlOptions = $httpRequester->options();
+		$this->assertEquals(
+			$testCase,
+			$curlOptions[ \CURLOPT_HTTPHEADER ][ 0 ],
+			'Could not retrieve header.'
+		);
+	}
+
+	public function test_setting_post_data()
+	{
+		$testCase = 'test 1234';
+		$httpRequester = new HttpRequester( NULL );
+		$httpRequester->setPostData( $testCase );
+		$curlOptions = $httpRequester->options();
+		// Verify POST flag was set.
+		$this->assertTrue(
+			$curlOptions[ \CURLOPT_POST ],
+			'Could not set post flag.'
+		);
+		// Verify POST data was set.
+		$this->assertEquals(
+			$testCase,
+			$curlOptions[ \CURLOPT_POSTFIELDS ],
+			'Could not set post data.'
+		);
+	}
+
+	/**
+	 * @expectedException \InvalidArgumentException
+	 * @expectedExceptionMessage POST data must be a string.
+	 */
+	public function test_setting_invalid_post_data()
+	{
+		$testCase = 'test 1234';
+		$httpRequester = new HttpRequester( NULL );
+		$httpRequester->setPostData([ $testCase ]);
+	}
+}
---
+++ src/Kshabazz/Slib/HttpRequester.php
@@ -0,0 +1,180 @@
+<?php namespace Kshabazz\Slib;
+/**
+ * Generic methods for retrieving HTML pages.
+ * Developers: Khalifah K. Shabazz
+ */
+
+/**
+ * Class HttpRequester
+ * Requires cURL extension
+ *
+ * @package Kshabazz\Slib
+ */
+class HttpRequester
+{
+	protected
+		$curl,
+		$curlOptions,
+		$headers,
+		$postData,
+		$requestInfo,
+		$responseText,
+		$url;
+
+	/**
+	 * Constructor
+	 */
+	public function __construct( $pUrl, array $pOptions = NULL )
+	{
+		$this->curlOptions = [ \CURLOPT_RETURNTRANSFER => TRUE ];
+		$this->headers = [ 'Content-type: text/html;' ];
+		$this->requestInfo = NULL;
+		$this->responseText = NULL;
+		$this->url = $pUrl;
+		// init curl.
+		$this->curl = curl_init( $this->url );
+	}
+
+	/**
+	 * Destructor
+	 */
+	public function __destruct()
+	{
+		// PHP does this auto, but I wanna make sure it gets done myself.
+		unset(
+			$this->curl,
+			$this->curlOptions,
+			$this->headers,
+			$this->requestInfo,
+			$this->postData,
+			$this->responseText,
+			$this->url
+		);
+	}
+
+	/**
+	 * Allow using this object for multiple URL request.
+	 *
+	 * @param $pUrl
+	 * @param $pOptions array replace previous curlOptions set, however, this will not unset any curlOptions in curl set by previous calls to send().
+	 * @return mixed|null
+	 * @throws \Exception
+	 */
+	public function request( $pUrl, $pOptions = NULL )
+	{
+		$returnValue = NULL;
+		if ( \isString($pUrl) )
+		{
+			$this->url = $pUrl;
+			// set curlOptions, replaces previous curlOptions.
+			if ( \isArray($pOptions) )
+			{
+				$this->curlOptions = $pOptions;
+			}
+			// Return the response text.
+			$returnValue = $this->send();
+		}
+		else
+		{
+			throw new \Exception( "Invalid URL '{$pUrl}'" );
+		}
+		return $returnValue;
+	}
+
+	/**
+	 * Get the HTTP response code of the request.
+	 * @return int HTTP response code.
+	 */
+	public function responseCode()
+	{
+		if ( \isArray($this->requestInfo) && array_key_exists("http_code", $this->requestInfo) )
+		{
+			return $this->requestInfo[ "http_code" ];
+		}
+		return NULL;
+	}
+
+	/**
+	 * Send an HTTP request
+	 * @return mixed|null
+	 * @throws \Exception
+	 */
+	public function send()
+	{
+		//
+		$returnValue = NULL;
+		if ( !empty($this->url) )
+		{
+			// set any curl curlOptions needed.
+			curl_setopt_array( $this->curl, $this->curlOptions );
+			// Send the request and get a response.
+			$responseText = \curl_exec( $this->curl );
+			// get the status of the call
+			$this->requestInfo = \curl_getinfo( $this->curl );
+			\curl_close( $this->curl );
+			if ( !empty($responseText) )
+			{
+				$returnValue = $responseText;
+			}
+		}
+		else
+		{
+			throw new \Exception( "Invalid URL: '{$this->url}'" );
+		}
+		return $returnValue;
+	}
+
+	/**
+	 * Set the URL of the request.
+	 */
+	public function setUrl( $pUrl )
+	{
+		$this->url = $pUrl;
+	}
+
+	/**
+	 * Set the URL of the request.
+	 */
+	public function headers( $pHeaders )
+	{
+		$this->headers = $pHeaders;
+		$this->curlOptions[ \CURLOPT_HTTPHEADER ] = $this->headers;
+		return $this->headers;
+	}
+
+	/**
+	 * Currently set cURL options.
+	 * @return array
+	 */
+	public function options()
+	{
+		return $this->curlOptions;
+	}
+
+	/**
+	 * Set the URL of the request.
+	 * @param $pData
+	 * @throws \InvalidArgumentException
+	 */
+	public function setPostData( $pData )
+	{
+		if ( !is_string($pData) )
+		{
+			throw new \InvalidArgumentException( 'POST data must be a string.' );
+		}
+		$this->postData = $pData;
+		$this->curlOptions[ \CURLOPT_POST ] = TRUE;
+		$this->curlOptions[ \CURLOPT_POSTFIELDS ] = $this->postData;
+	}
+
+	/**
+	 * Get the URL of the request.
+	 *
+	 * @return {string|NULL}
+	 */
+	public function url()
+	{
+		return $this->url;
+	}
+}
+?>
